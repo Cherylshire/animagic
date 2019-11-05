73640b2b4d0c084970a49928c62dcf6aac2db6e7
“Cherylshire” <hsuanchang2017@gmail.com>
2019-11-05 13:01:54 -0600

    added validation

--- app/models/image_order.rb
+++ app/models/image_order.rb
@@ -3,4 +3,7 @@ class ImageOrder < ApplicationRecord
   belongs_to :image
 
   validates :placement, uniqueness: {scope: :image_id, message: "can only input one image placement per image!"}
+  validates :placement, length: { in: 1..10 }
+  validates :placement, format: { with: /\d(1..10)/,
+    message: "only 1 through 10." }
 end
