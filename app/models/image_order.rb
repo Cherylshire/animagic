8f1e01089400b1e36987c46e8d60a333e05534a4
“Cherylshire” <hsuanchang2017@gmail.com>
2019-11-05 16:30:15 -0600

    Tier_2_done

--- app/models/image_order.rb
+++ app/models/image_order.rb
@@ -3,7 +3,6 @@ class ImageOrder < ApplicationRecord
   belongs_to :image
 
   validates :placement, uniqueness: {scope: :image_id, message: "can only input one image placement per image!"}
+  validates :image_id, uniqueness: {scope: :user_id, message: "can only input an image once per user."}
   validates :placement, length: { in: 1..10 }
-  validates :placement, format: { with: /\d(1..10)/,
-    message: "only 1 through 10." }
 end
